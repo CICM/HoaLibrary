508c78a9c9d6eabb260177ce99b7b1be7caad845
Guillot Pierre <guillotpierre6@gmail.com>
2014-03-03 17:13:24 +0100

    HarmoNickNumber

--- Max/Max2D/HoaOptim/hoa.2d.optim~.cpp
+++ Max/Max2D/HoaOptim/hoa.2d.optim~.cpp
@@ -4,7 +4,7 @@
 // WARRANTIES, see the file, "LICENSE.txt," in this distribution.
 */
 
-#include "../../Sources/Hoa2D/Hoa2D.h"
+#include "../Hoa2D.max.h"
 
 extern "C"
 {
@@ -28,7 +28,12 @@ void hoa_optim_assist(t_hoa_optim *x, void *b, long m, long a, char *s);
 
 void hoa_optim_dsp64(t_hoa_optim *x, t_object *dsp64, short *count, double samplerate, long maxvectorsize, long flags);
 void hoa_optim_perform64(t_hoa_optim *x, t_object *dsp64, double **ins, long numins, double **outs, long numouts, long sampleframes, long flags, void *userparam);
-void hoa_optim_perform64_wide(t_hoa_optim *x, t_object *dsp64, double **ins, long numins, double **outs, long numouts, long sampleframes, long flags, void *userparam);
+
+void hoa_optim_basic(t_hoa_optim *x);
+void hoa_optim_maxre(t_hoa_optim *x);
+void hoa_optim_inphase(t_hoa_optim *x);
+
+t_hoa_err hoa_getinfos(t_hoa_optim* x, t_hoa_boxinfos* boxinfos);
 
 t_class *hoa_optim_class;
     
@@ -38,11 +43,16 @@ int C74_EXPORT main(void)
 
 	t_class *c;
 	
-	c = class_new("hoa.optim~", (method)hoa_optim_new, (method)hoa_optim_free, (long)sizeof(t_hoa_optim), 0L, A_GIMME, 0);
+	c = class_new("hoa.2d.optim~", (method)hoa_optim_new, (method)hoa_optim_free, (long)sizeof(t_hoa_optim), 0L, A_GIMME, 0);
+    class_alias(c, gensym("hoa.optim~"));
 	
+    hoa_initclass(c, (method)hoa_getinfos);
 	class_addmethod(c, (method)hoa_optim_dsp64,		"dsp64",	A_CANT, 0);
 	class_addmethod(c, (method)hoa_optim_assist,    "assist",	A_CANT, 0);
-	
+	class_addmethod(c, (method)hoa_optim_basic,     "basic",	A_NOTHING, 0);
+    class_addmethod(c, (method)hoa_optim_maxre,     "maxRe",	A_NOTHING, 0);
+    class_addmethod(c, (method)hoa_optim_inphase,   "inPhase",	A_NOTHING, 0);
+    
 	class_dspinit(c);
 	class_register(CLASS_BOX, c);	
 	hoa_optim_class = c;
@@ -55,49 +65,48 @@ int C74_EXPORT main(void)
 void *hoa_optim_new(t_symbol *s, long argc, t_atom *argv)
 {
 	t_hoa_optim *x = NULL;
-	int	order = 4;
+	int	order = 1;
     x = (t_hoa_optim *)object_alloc(hoa_optim_class);
 	if (x)
 	{		
 		if(atom_gettype(argv) == A_LONG)
 			order = atom_getlong(argv);
-		if(atom_gettype(argv+1) == A_LONG && atom_getlong(argv+1) == 1)
-            x->f_optim = new Hoa2D::Optim(order, Hoa2D::Optim::MaxRe);
-        else
-            x->f_optim = new Hoa2D::Optim(order, Hoa2D::Optim::inPhase);
+        if(order < 1)
+            order = 1;
+        
+        x->f_optim = new Hoa2D::Optim(order, Hoa2D::Optim::InPhase);
 		
-		dsp_setup((t_pxobject *)x, x->f_optim->getNumberOfInputs());
-		for (int i = 0; i < x->f_optim->getNumberOfOutputs(); i++)
+        if(argc > 1 && atom_gettype(argv+1) == A_SYM)
+        {
+            if(atom_getsym(argv+1) == gensym("maxRe"))
+                x->f_optim->setMode(Hoa2D::Optim::MaxRe);
+            else if(atom_getsym(argv+1) == gensym("basic"))
+                x->f_optim->setMode(Hoa2D::Optim::Basic);
+        }
+        
+		dsp_setup((t_pxobject *)x, x->f_optim->getNumberOfHarmonics());
+		for (int i = 0; i < x->f_optim->getNumberOfHarmonics(); i++)
 			outlet_new(x, "signal");
         
-        x->f_signals =  new double[x->f_optim->getNumberOfOutputs() * SYS_MAXBLKSIZE];
+        x->f_signals =  new double[x->f_optim->getNumberOfHarmonics() * SYS_MAXBLKSIZE];
 	}
 
 	return (x);
 }
 
-void hoa_optim_dsp64(t_hoa_optim *x, t_object *dsp64, short *count, double samplerate, long maxvectorsize, long flags)
+t_hoa_err hoa_getinfos(t_hoa_optim* x, t_hoa_boxinfos* boxinfos)
 {
-    if(count[x->f_optim->getNumberOfInputs() - 1])
-        object_method(dsp64, gensym("dsp_add64"), x, hoa_optim_perform64_wide, 0, NULL);
-    else
-        object_method(dsp64, gensym("dsp_add64"), x, hoa_optim_perform64, 0, NULL);
+	boxinfos->object_type = HOA_OBJECT_2D;
+	boxinfos->autoconnect_inputs = x->f_optim->getNumberOfHarmonics();
+	boxinfos->autoconnect_outputs = x->f_optim->getNumberOfHarmonics();
+	boxinfos->autoconnect_inputs_type = HOA_CONNECT_TYPE_AMBISONICS;
+	boxinfos->autoconnect_outputs_type = HOA_CONNECT_TYPE_AMBISONICS;
+	return HOA_ERR_NONE;
 }
 
-void hoa_optim_perform64_wide(t_hoa_optim *x, t_object *dsp64, double **ins, long numins, double **outs, long numouts, long sampleframes, long flags, void *userparam)
+void hoa_optim_dsp64(t_hoa_optim *x, t_object *dsp64, short *count, double samplerate, long maxvectorsize, long flags)
 {
-    for(int i = 0; i < numouts; i++)
-    {
-        cblas_dcopy(sampleframes, ins[i], 1, x->f_signals+i, numins);
-    }
-	for(int i = 0; i < sampleframes; i++)
-    {
-        x->f_optim->process(x->f_signals + numouts * i, x->f_signals + numouts * i);
-    }
-    for(int i = 0; i < numouts; i++)
-    {
-        cblas_dcopy(sampleframes, x->f_signals+i, numouts, outs[i], 1);
-    }
+    object_method(dsp64, gensym("dsp_add64"), x, hoa_optim_perform64, 0, NULL);
 }
 
 void hoa_optim_perform64(t_hoa_optim *x, t_object *dsp64, double **ins, long numins, double **outs, long numouts, long sampleframes, long flags, void *userparam)
@@ -121,6 +130,23 @@ void hoa_optim_assist(t_hoa_optim *x, void *b, long m, long a, char *s)
 	sprintf(s,"(Signal) %s", x->f_optim->getHarmonicsName(a).c_str());
 }
 
+void hoa_optim_basic(t_hoa_optim *x)
+{
+    if(x->f_optim->getMode() != Hoa2D::Optim::Basic)
+        x->f_optim->setMode(Hoa2D::Optim::Basic);
+}
+
+void hoa_optim_maxre(t_hoa_optim *x)
+{
+    if(x->f_optim->getMode() != Hoa2D::Optim::MaxRe)
+        x->f_optim->setMode(Hoa2D::Optim::MaxRe);
+}
+
+void hoa_optim_inphase(t_hoa_optim *x)
+{
+    if(x->f_optim->getMode() != Hoa2D::Optim::InPhase)
+        x->f_optim->setMode(Hoa2D::Optim::InPhase);
+}
 
 void hoa_optim_free(t_hoa_optim *x) 
 {
