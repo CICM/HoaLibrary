94fb0f68ebe0ae27ddb521dfd33988ac3da8b555
Guillot Pierre <guillotpierre6@gmail.com>
2014-03-06 16:50:48 +0100

    clean...

--- Documentation/html/search/all_61.js
+++ Documentation/html/search/all_61.js
@@ -2,8 +2,7 @@ var searchData=
 [
   ['ambisonic',['Ambisonic',['../class_hoa2_d_1_1_ambisonic.html',1,'Hoa2D']]],
   ['ambisonic',['Ambisonic',['../class_hoa3_d_1_1_ambisonic.html',1,'Hoa3D']]],
-  ['ambisonic',['Ambisonic',['../class_hoa2_d_1_1_ambisonic.html#a43f91832422c0b59f92f02978dc1e541',1,'Hoa2D::Ambisonic::Ambisonic()'],['../class_hoa3_d_1_1_ambisonic.html#a43f91832422c0b59f92f02978dc1e541',1,'Hoa3D::Ambisonic::Ambisonic()']]],
-  ['ambisonicsbinaural',['AmbisonicsBinaural',['../class_ambisonics_binaural.html',1,'AmbisonicsBinaural'],['../class_ambisonics_binaural.html#aa3154f11cb7a385b09a743c5e46ee2e9',1,'AmbisonicsBinaural::AmbisonicsBinaural()']]],
+  ['ambisonic',['Ambisonic',['../class_hoa2_d_1_1_ambisonic.html#ac53cc3ea0b6600dd84d761f89b5af9d3',1,'Hoa2D::Ambisonic::Ambisonic()'],['../class_hoa3_d_1_1_ambisonic.html#a34736bb8f9bb34ad4a3844258bbac2fd',1,'Hoa3D::Ambisonic::Ambisonic()']]],
   ['ambisonicsmeter',['AmbisonicsMeter',['../class_ambisonics_meter.html',1,'AmbisonicsMeter'],['../class_ambisonics_meter.html#ab6652775a732fc5d190101e5673c6758',1,'AmbisonicsMeter::AmbisonicsMeter()']]],
   ['ambisonicvirtualmicui',['AmbisonicVirtualMicUI',['../class_ambisonic_virtual_mic_u_i.html',1,'']]],
   ['ambisonicvirtualmicuimanager',['AmbisonicVirtualMicUIManager',['../class_ambisonic_virtual_mic_u_i_manager.html',1,'']]],
--- Documentation/html/search/functions_61.js
+++ Documentation/html/search/functions_61.js
@@ -1,7 +1,6 @@
 var searchData=
 [
-  ['ambisonic',['Ambisonic',['../class_hoa2_d_1_1_ambisonic.html#a43f91832422c0b59f92f02978dc1e541',1,'Hoa2D::Ambisonic::Ambisonic()'],['../class_hoa3_d_1_1_ambisonic.html#a43f91832422c0b59f92f02978dc1e541',1,'Hoa3D::Ambisonic::Ambisonic()']]],
-  ['ambisonicsbinaural',['AmbisonicsBinaural',['../class_ambisonics_binaural.html#aa3154f11cb7a385b09a743c5e46ee2e9',1,'AmbisonicsBinaural']]],
+  ['ambisonic',['Ambisonic',['../class_hoa2_d_1_1_ambisonic.html#ac53cc3ea0b6600dd84d761f89b5af9d3',1,'Hoa2D::Ambisonic::Ambisonic()'],['../class_hoa3_d_1_1_ambisonic.html#a34736bb8f9bb34ad4a3844258bbac2fd',1,'Hoa3D::Ambisonic::Ambisonic()']]],
   ['ambisonicsmeter',['AmbisonicsMeter',['../class_ambisonics_meter.html#ab6652775a732fc5d190101e5673c6758',1,'AmbisonicsMeter']]],
   ['associated_5flegendre',['associated_legendre',['../namespace_hoa.html#ad044777140feb80ca758bb11ad1173d1',1,'Hoa::associated_legendre()'],['../namespace_hoa3_d.html#af20cfa7da792b7bb0b6f98eccb371ca8',1,'Hoa3D::associated_legendre()']]]
 ];
