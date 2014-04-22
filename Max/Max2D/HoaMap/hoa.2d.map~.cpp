0d8138432244b459650c5d11ed27624ac2aab93f
eliott paris <eliottparis@gmail.com>
2014-04-22 17:57:35 +0200

    Map issue fixed !

--- Max/Max2D/HoaMap/hoa.2d.map~.cpp
+++ Max/Max2D/HoaMap/hoa.2d.map~.cpp
@@ -27,66 +27,22 @@
  */
 
 #include "../Hoa2D.max.h"
+#include "MaxLineMap.h"
 
 #define MAX_NUMBER_OF_SOURCES 64
-
-class PolarLines
-{
-    
-private:
-    float*      m_values_old;
-    float*      m_values_new;
-    float*      m_values_step;
-    unsigned int m_counter;
-    unsigned int m_ramp;
-    unsigned int m_number_of_sources;
-    
-public:
-    PolarLines(unsigned int numberOfSources);
-    ~PolarLines();
-    
-    inline unsigned int getNumberOfSources() const
-    {
-        return m_number_of_sources;
-    }
-    
-    inline unsigned int getRamp() const
-    {
-        return m_ramp;
-    }
-    
-    inline double getRadius(unsigned int index) const
-    {
-        assert(index < m_number_of_sources);
-        return m_values_new[index];
-    }
-    
-    inline double getAzimuth(unsigned int index) const
-    {
-        assert(index < m_number_of_sources);
-        return m_values_new[m_number_of_sources +index];
-    }
-    
-    void setRamp(unsigned int ramp);
-    void setRadius(unsigned int index, double radius);
-    void setAzimuth(unsigned int index, double azimuth);
-    void setRadiusDirect(unsigned int index, double radius);
-    void setAzimuthDirect(unsigned int index, double azimuth);
-    
-    void process(float* vector);
-};
 
 typedef struct _hoa_map
 {
 	t_pxobject      f_ob;
+	PolarLines*     f_lines;
 	Hoa2D::Map*     f_map;
-    double*         f_sig_ins;
+	
+    float			f_lines_vector[128];
+	double*         f_sig_ins;
     double*         f_sig_outs;
-    
+	
 	t_symbol*       f_mode;
     double          f_ramp;
-    PolarLines*     f_lines;
-    float*          f_lines_vector;
 } t_hoa_map;
 
 void *hoa_map_new(t_symbol *s, long argc, t_atom *argv);
@@ -183,6 +139,7 @@ void *hoa_map_new(t_symbol *s, long argc, t_atom *argv)
 	int	order = 1;
     int numberOfSources = 1;
     t_symbol* temp_mode;
+	
     x = (t_hoa_map *)object_alloc(hoa_map_class);
 	if (x)
 	{
@@ -213,6 +170,9 @@ void *hoa_map_new(t_symbol *s, long argc, t_atom *argv)
             x->f_lines->setAzimuthDirect(i, 0.);
         }
         
+		x->f_sig_ins    = new double[MAX_NUMBER_OF_SOURCES * 8192];
+        x->f_sig_outs   = new double[MAX_NUMBER_OF_SOURCES * 8192];
+		
 		if(x->f_map->getNumberOfSources() == 1)
             dsp_setup((t_pxobject *)x, 3);
         else
@@ -220,16 +180,8 @@ void *hoa_map_new(t_symbol *s, long argc, t_atom *argv)
 		
 		for (int i = 0; i < x->f_map->getNumberOfHarmonics(); i++)
 			outlet_new(x, "signal");
-        
-        if(x->f_map->getNumberOfSources() == 1)
-            x->f_sig_ins    = new double[3 * sys_getblksize()];
-        else
-            x->f_sig_ins    = new double[x->f_map->getNumberOfSources() * sys_getblksize()];
-		
-        x->f_sig_outs       = new double[x->f_map->getNumberOfHarmonics() * sys_getblksize()];
-        x->f_lines_vector   = new float[x->f_map->getNumberOfSources() * 2];
 	}
-
+	
 	return (x);
 }
 
@@ -348,7 +300,7 @@ void hoa_map_dsp64(t_hoa_map *x, t_object *dsp64, short *count, double samplerat
 
 void hoa_map_perform64_multisources(t_hoa_map *x, t_object *dsp64, double **ins, long numins, double **outs, long numouts, long sampleframes, long flags, void *userparam)
 {
-    int nsources = x->f_map->getNumberOfSources();
+    int nsources = clip_max(x->f_map->getNumberOfSources(), MAX_NUMBER_OF_SOURCES);
     for(int i = 0; i < numins; i++)
     {
         cblas_dcopy(sampleframes, ins[i], 1, x->f_sig_ins+i, numins);
@@ -356,11 +308,12 @@ void hoa_map_perform64_multisources(t_hoa_map *x, t_object *dsp64, double **ins,
     for(int i = 0; i < sampleframes; i++)
     {
         x->f_lines->process(x->f_lines_vector);
+		
 		for(int j = 0; j < nsources; j++)
 			x->f_map->setRadius(j, x->f_lines_vector[j]);
-        for(int j = 0; j < nsources; j++)
+		for(int j = 0; j < nsources; j++)
 			x->f_map->setAzimuth(j, x->f_lines_vector[j + nsources]);
-        
+		
         x->f_map->process(x->f_sig_ins + numins * i, x->f_sig_outs + numouts * i);
     }
     for(int i = 0; i < numouts; i++)
@@ -504,93 +457,6 @@ void hoa_map_free(t_hoa_map *x)
 	delete x->f_map;
     delete [] x->f_sig_ins;
     delete [] x->f_sig_outs;
-	delete [] x->f_lines_vector;
 	delete x->f_lines;
-}
-
-PolarLines::PolarLines(unsigned int numberOfSources)
-{
-    assert(numberOfSources > 0);
-    m_number_of_sources = numberOfSources;
-    
-    m_values_old    = new float[m_number_of_sources * 2];
-    m_values_new    = new float[m_number_of_sources * 2];
-    m_values_step   = new float[m_number_of_sources * 2];
-}
-
-PolarLines::~PolarLines()
-{
-    delete [] m_values_old;
-    delete [] m_values_new;
-    delete [] m_values_step;
-}
-
-void PolarLines::setRamp(unsigned int ramp)
-{
-    m_ramp = clip_min(ramp, (long)1);
-}
-
-void PolarLines::setRadius(unsigned int index, double radius)
-{
-    assert(index < m_number_of_sources);
-    m_values_new[index]  = radius;
-    m_values_step[index] = (m_values_new[index] - m_values_old[index]) / (double)m_ramp;
-    m_counter = 0;
-}
-
-void PolarLines::setAzimuth(unsigned int index, double azimuth)
-{
-    assert(index < m_number_of_sources);
-    m_values_new[index + m_number_of_sources] = wrap_twopi(azimuth);
-    m_values_old[index + m_number_of_sources] = wrap_twopi(m_values_old[index + m_number_of_sources]);
-    double distance;
-    if(m_values_old[index + m_number_of_sources] > m_values_new[index + m_number_of_sources])
-        distance = (m_values_old[index + m_number_of_sources] - m_values_new[index + m_number_of_sources]);
-    else
-        distance = (m_values_new[index + m_number_of_sources] - m_values_old[index + m_number_of_sources]);
-    if(distance <= HOA_PI)
-    {
-        m_values_step[index + m_number_of_sources] = (m_values_new[index + m_number_of_sources] - m_values_old[index + m_number_of_sources]) / (double)m_ramp;
-    }
-    else
-    {
-        if(m_values_new > m_values_old)
-        {
-            m_values_step[index + m_number_of_sources] = ((m_values_new[index + m_number_of_sources] - HOA_2PI) - m_values_old[index + m_number_of_sources]) / (double)m_ramp;
-        }
-        else
-        {
-            m_values_step[index + m_number_of_sources] = ((m_values_new[index + m_number_of_sources] + HOA_2PI) - m_values_old[index + m_number_of_sources]) / (double)m_ramp;
-        }
-    }
-    m_counter = 0;
-}
-
-void PolarLines::setRadiusDirect(unsigned int index, double radius)
-{
-    assert(index < m_number_of_sources);
-    m_values_old[index] = m_values_new[index] = radius;
-    m_values_step[index] = 0.;
-    m_counter = 0;
-}
-
-void PolarLines::setAzimuthDirect(unsigned int index, double azimuth)
-{
-    assert(index < m_number_of_sources);
-    m_values_old[index + m_number_of_sources] = m_values_new[index + m_number_of_sources] = azimuth;
-    m_values_step[index + m_number_of_sources] = 0.;
-    m_counter = 0;
-}
-
-void PolarLines::process(float* vector)
-{
-    cblas_saxpy(m_number_of_sources * 2, 1., m_values_step, 1, m_values_old, 1);
-    if(m_counter++ >= m_ramp)
-    {
-        cblas_scopy(m_number_of_sources * 2, m_values_new, 1, m_values_old, 1);
-        memset(m_values_step, 0, sizeof(float) * m_number_of_sources * 2);
-        m_counter    = 0;
-    }
-    cblas_scopy(m_number_of_sources * 2, m_values_old, 1, vector, 1);
 }
 
